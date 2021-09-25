312130784b1ace71d21137e9e1a909b7655a5cac
MyHanhYuuki <MyHanhPhung0302@gmail.com>
2021-09-26 01:44:31 +0700

    Tro Choi Doan So

---
+++ Cau-15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cau_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" TRO CHOI DOAN SO \n");
+            Console.WriteLine(" \n****************\n");
+            Console.WriteLine(" Nhap gia tri nho nhat: \n");
+            int minValue = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Nhap gia tri lon nhat: \n");
+            int maxValue = int.Parse(Console.ReadLine());
+
+            Random myObject = new Random();
+            int ranNum = myObject.Next(minValue, maxValue);
+
+            Console.WriteLine(" Nhap so du doan: \n");
+            int Guess = int.Parse(Console.ReadLine());
+            while (Guess != ranNum)
+            {
+                if (Guess < ranNum)
+                    Console.WriteLine(">" + Guess);
+                else
+                    Console.WriteLine("<" + Guess);
+
+                Console.WriteLine(" Nhap so du doan: \n");
+                Guess = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("  Chuc mung ban da doan chinh xac ! ");
+        }
+
+
+
+
+    }
+}
