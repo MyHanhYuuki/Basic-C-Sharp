eddee2e1ffb08a7ce9e48f5db26694a20e82e176
MyHanhYuuki <MyHanhPhung0302@gmail.com>
2021-10-10 17:30:00 +0700

    Menu lua chon cac phep toan co ban

--- Cau-17/Program.cs
+++ Cau-17/Program.cs
@@ -12,16 +12,16 @@ namespace Cau_17
             int num1, num2, opt;
 
             Console.Write("\n");
-            Console.Write("Hien thi menu co cac lua chon de thuc hien cac phep toan co ban:\n");
-            Console.Write("--------------------------------------------------------------------");
+            Console.Write("Hien thi menu voi cac lua chon de thuc hien cac phep toan co ban( + - x :) :\n");
+            Console.Write("-------------------------------------------------------------------------------");
             Console.Write("\n\n");
 
-            Console.Write("Nhap so nguyen thu nhat: ");
+            Console.Write("Ban hay nhap so nguyen thu nhat: ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Nhap so nguyen thu hai: ");
+            Console.Write("Ban hay nhap so nguyen thu hai: ");
             num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("\nDuoi day la cac lua chon:\n");
+            Console.Write("\nLua chon cac phep tinh:\n");
             Console.Write("1 - Phep cong.\n2 - Phep tru.\n3 - Phep nhan.\n4 - Phep chia.\n5 - Thoat.\n");
             Console.Write("\nNhap lua chon cua ban: ");
             opt = Convert.ToInt32(Console.ReadLine());
@@ -29,15 +29,15 @@ namespace Cau_17
             switch (opt)
             {
                 case 1:
-                    Console.Write("Tong hai so {0} va {1} la: {2}\n", num1, num2, num1 + num2);
+                    Console.Write("-> Tong cua hai so {0} va {1} la: {2}\n", num1, num2, num1 + num2);
                     break;
 
                 case 2:
-                    Console.Write("Hieu cua {0} va {1} la: {2}\n", num1, num2, num1 - num2);
+                    Console.Write("-> Hieu cua hai so {0} va {1} la: {2}\n", num1, num2, num1 - num2);
                     break;
 
                 case 3:
-                    Console.Write("Tich cua {0} va {1} la: {2}\n", num1, num2, num1 * num2);
+                    Console.Write("-> Tich cua hai so {0} va {1} la: {2}\n", num1, num2, num1 * num2);
                     break;
 
                 case 4:
@@ -47,7 +47,7 @@ namespace Cau_17
                     }
                     else
                     {
-                        Console.Write("Thuong cua {0} va {1} la: {2}\n", num1, num2, num1 / num2);
+                        Console.Write("-> Thuong cua hai so {0} va {1} la: {2}\n", num1, num2, num1 / num2);
                     }
                     break;
 
