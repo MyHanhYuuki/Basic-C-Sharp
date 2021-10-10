e4eac3907ea6beba42191ba888b0bdfa4e8594e0
MyHanhYuuki <MyHanhPhung0302@gmail.com>
2021-10-10 17:36:24 +0700

    Tim UCLN và BCNN cua 2 so

--- Cau-19/Program.cs
+++ Cau-19/Program.cs
@@ -6,18 +6,18 @@ namespace Cau_19
 {
     class Program
     {
-        // Ham uoc so chung lon nhat (USCLN)
-        static int USCLN(int a, int b)
+        // Ham uoc chung lon nhat (UCLN)
+        static int UCLN(int a, int b)
         {
                 if (b == 0) return a;
-                return USCLN(b, a % b);
+                return UCLN(b, a % b);
         }
 
 
-        // Ham boi so chung nho nhat (BSCNN)         
-        static int BSCNN(int a, int b)
+        // Ham boi chung nho nhat (BCNN)         
+        static int BCNN(int a, int b)
         { 
-               return (a * b) / USCLN(a, b);
+               return (a * b) / UCLN(a, b);
         }
             
         static void Main(string[] args)
@@ -28,10 +28,10 @@ namespace Cau_19
                 Console.Write("Ban hay nhap vao so nguyen duong b = ");
                 String valB = Console.ReadLine();
                 int b = Convert.ToInt32(valB);
-                // tinh USCLN cua a và b
-                Console.Write("USCLN cua {0} va {1} la: {2} \n", a, b, USCLN(a, b));
-                // tinh BSCNN cua a và b
-                Console.Write("USCLN cua cua {0} va {1} la: {2}", a, b, BSCNN(a, b));
+              
+                Console.Write("UCLN cua {0} va {1} la: {2} \n", a, b, UCLN(a, b));          
+                Console.Write("BCNN cua {0} va {1} la: {2} \n", a, b, BCNN(a, b));
+                Console.ReadKey();
         }    
     }  
 }
