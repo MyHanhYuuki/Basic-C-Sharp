9339480abe69a7b7bd7cf192abd437a298ce4488
MyHanhYuuki <MyHanhPhung0302@gmail.com>
2021-11-21 02:43:47 +0700

    Chuong trinh quan ly thong tin sinh vien co ban

---
+++ Bai_05/Form2.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Bai_05
+{
+    partial class FormAddStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelDepartment = new System.Windows.Forms.Label();
+            this.labelGPA = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxGPA = new System.Windows.Forms.TextBox();
+            this.comboBoxDepartment = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("Times New Roman", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelID.Location = new System.Drawing.Point(136, 78);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(115, 19);
+            this.labelID.TabIndex = 0;
+            this.labelID.Text = "Mã số sinh viên";
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Times New Roman", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(152, 123);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(98, 19);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Tên sinh viên";
+            // 
+            // labelDepartment
+            // 
+            this.labelDepartment.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDepartment.AutoSize = true;
+            this.labelDepartment.Font = new System.Drawing.Font("Times New Roman", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDepartment.Location = new System.Drawing.Point(211, 167);
+            this.labelDepartment.Name = "labelDepartment";
+            this.labelDepartment.Size = new System.Drawing.Size(45, 19);
+            this.labelDepartment.TabIndex = 0;
+            this.labelDepartment.Text = "Khoa";
+            // 
+            // labelGPA
+            // 
+            this.labelGPA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelGPA.AutoSize = true;
+            this.labelGPA.Font = new System.Drawing.Font("Times New Roman", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGPA.Location = new System.Drawing.Point(132, 214);
+            this.labelGPA.Name = "labelGPA";
+            this.labelGPA.Size = new System.Drawing.Size(119, 19);
+            this.labelGPA.TabIndex = 0;
+            this.labelGPA.Text = "Điểm trung bình";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxID.Location = new System.Drawing.Point(302, 78);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(305, 23);
+            this.textBoxID.TabIndex = 1;
+            this.textBoxID.TextChanged += new System.EventHandler(this.textBoxID_TextChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(302, 123);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(305, 23);
+            this.textBoxName.TabIndex = 1;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxGPA
+            // 
+            this.textBoxGPA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxGPA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxGPA.Location = new System.Drawing.Point(302, 210);
+            this.textBoxGPA.Name = "textBoxGPA";
+            this.textBoxGPA.Size = new System.Drawing.Size(144, 23);
+            this.textBoxGPA.TabIndex = 1;
+            this.textBoxGPA.TextChanged += new System.EventHandler(this.textBoxGPA_TextChanged);
+            // 
+            // comboBoxDepartment
+            // 
+            this.comboBoxDepartment.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxDepartment.FormattingEnabled = true;
+            this.comboBoxDepartment.Items.AddRange(new object[] {
+            "Công nghệ Thông tin",
+            "Hệ thống Thông tin",
+            "Khoa học Máy tính",
+            "Kỹ thuật Phần mềm",
+            "Kỹ thuật Máy tính",
+            "Mạng máy tính và An toàn thông tin",
+            "Mạng máy tính và Truyền thông dữ liệu",
+            "An toàn thông tin",
+            "Thương mại điện tử",
+            "Khoa học Dữ liệu"});
+            this.comboBoxDepartment.Location = new System.Drawing.Point(302, 166);
+            this.comboBoxDepartment.Name = "comboBoxDepartment";
+            this.comboBoxDepartment.Size = new System.Drawing.Size(305, 25);
+            this.comboBoxDepartment.TabIndex = 2;
+            this.comboBoxDepartment.TabStop = false;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.BackColor = System.Drawing.Color.SeaGreen;
+            this.buttonAdd.Enabled = false;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(436, 331);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(104, 32);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Thêm mới";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.BackColor = System.Drawing.Color.Tomato;
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(566, 331);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(104, 32);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Thoát";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // FormAddStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(726, 399);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.comboBoxDepartment);
+            this.Controls.Add(this.textBoxGPA);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelGPA);
+            this.Controls.Add(this.labelDepartment);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelID);
+            this.Name = "FormAddStudent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thêm sinh viên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelDepartment;
+        private System.Windows.Forms.Label labelGPA;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxGPA;
+        private System.Windows.Forms.ComboBox comboBoxDepartment;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ Bai_05/Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_05
+{
+    public partial class FormAddStudent : Form
+    {
+        public FormAddStudent()
+        {
+            InitializeComponent();
+            textBoxID.Select();
+            textBoxID.Focus();
+            comboBoxDepartment.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxDepartment.SelectedIndex = 0;
+        }
+
+        private bool AreControlsValid(Control.ControlCollection controls)
+        {
+            foreach (Control c in controls)
+            {
+                if (c is TextBox tb && c.Visible)
+                {
+                    if (string.IsNullOrEmpty(tb.Text))
+                        return false;
+                }
+
+                if (c.HasChildren)
+                    AreControlsValid(c.Controls);
+            }
+            return true;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            bool valid = double.TryParse(textBoxGPA.Text, out double value);
+            if (valid)
+            {
+                FormMain.students.Add(new Student(textBoxID.Text, textBoxName.Text, comboBoxDepartment.SelectedItem.ToString(), value));
+                FormMain.dgv.Rows.Clear();
+                for (int i = 0; i < FormMain.students.Count; i++)
+                {
+                    FormMain.dgv.Rows.Add((i + 1).ToString(), FormMain.students[i].ID, FormMain.students[i].Name, FormMain.students[i].Department, FormMain.students[i].GPA);
+                }
+                Close();
+                FormMain.instance.Show();
+            }
+            else
+            {
+                MessageBox.Show("Diem trung binh khong hop le!", "Loi");
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+            FormMain.instance.Show();
+        }
+
+        private void textBoxID_TextChanged(object sender, EventArgs e)
+        {
+            buttonAdd.Enabled = AreControlsValid(Controls);
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            buttonAdd.Enabled = AreControlsValid(Controls);
+        }
+
+        private void textBoxGPA_TextChanged(object sender, EventArgs e)
+        {
+            buttonAdd.Enabled = AreControlsValid(Controls);
+        }
+    }
+}
