c6ae24dfabacd052e889ac6a419bf17f9e8a39e8
MyHanhYuuki <MyHanhPhung0302@gmail.com>
2021-11-21 02:39:30 +0700

    Winform hien thi ngay gio tren he thong

---
+++ Bai_02/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Bai_02
+{
+    public partial class Form_Clock : Form
+    {
+        public Form_Clock()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox_Clock.Text = DateTime.Today.DayOfWeek.ToString() + ", " + DateTime.Now.ToString();
+        }
+
+        private void Form_Clock_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
